40c8bdf7f6f564f6a1db0249290e9a61dda9fe32
dallinwm <98115987+dallinwm@users.noreply.github.com>
2022-07-02 17:21:44 -0700

    Deleted file, unneeded for deployment

--- Params.py
+++
@@ -1,7 +0,0 @@
-tag_list = ['WE1_PI_OV_ZONE_A_TEMP', 'WE1_PI_OV_ZONE_B_TEMP', 'WE1_PI_OV_FAN_ZA_SPD', 'WE1_NO_CHUTES']
-a_max_temp = 600
-b_max_temp = 600
-a_max_fan = 1167
-b_max_fan = 1167
-timestamp = '1-JUN-22 00:00'
-period = '00:05'
